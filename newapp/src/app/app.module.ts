4bb4b31da88ebbad34249d94b0bce0b22a19e378
Emmanuel Mascarinas <emmanuel_mascarinas@apple.com>
2017-01-19 16:50:52 -0800

    sdf

--- newapp/src/app/app.module.ts
+++ newapp/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { TabsModule } from 'ng2-bootstrap/tabs';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
